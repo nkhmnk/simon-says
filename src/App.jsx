661628307350ac53cc608adf1737348f659a0ba6
Nadiia Khomenko <ipz234_khnv@student.ztu.edu.ua>
2026-02-28 21:30:34 +0200

    Налаштування юридичних стандартів: ліцензія MIT та політика GDPR (Cookie Popup)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import StartPage from './pages/StartPage/StartPage';
 import GamePage from './pages/GamePage/GamePage';
 import ResultPage from './pages/ResultPage/ResultPage';
 import "./App.css";
+import CookieConsent from "react-cookie-consent";
 
 function App() {
   const [page, setPage] = useState("start");
@@ -39,6 +40,18 @@ function App() {
           onMain={goToStart} 
         />
       )}
+
+      <CookieConsent
+  location="bottom"
+  buttonText="Прийняти"
+  cookieName="simonSaysCookieConsent"
+  style={{ background: "#2B373B", color: "#fff" }}
+  buttonStyle={{ color: "#4e503b", fontSize: "13px", borderRadius: "5px" }}
+  expires={150}
+>
+  Цей сайт використовує локальне сховище для збереження ваших рекордів. Продовжуючи гру, ви погоджуєтеся з нашою{" "}
+  <a href="/PRIVACY_POLICY.md" style={{ color: "#ffd700" }}>Політикою конфіденційності</a>.
+</CookieConsent>
     </div>
   );
 }
